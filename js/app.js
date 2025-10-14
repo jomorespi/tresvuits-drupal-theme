f0449d8a8d3d8bad064a171779f4d46daab3aa78
Jordi <hola@jordimoreno.cat>
2025-10-14 14:47:00 +0200

    Main menu toggle

--- js/app.js
+++ js/app.js
@@ -715,6 +715,31 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"8lRBv":[function(require,module,exports,__globalThis) {
 window.addEventListener('DOMContentLoaded', function() {
+    // Main Navigation Toggle
+    const mainNav = document.querySelector('.tv-main-nav'), mainNavWrapper = document.querySelector('.tv-main-nav__wrapper'), menuBtn = document.querySelector('.tv-main-nav__toggle'), menuBtnTxt = menuBtn ? menuBtn.querySelector('.tv-main-nav__toggle__txt') : null, mainNavHeight = mainNav.offsetHeight, mainNavLinks = mainNav.querySelectorAll('a');
+    if (menuBtn && mainNav) menuBtn.addEventListener('click', function() {
+        if (mainNavWrapper.classList.contains('h-0')) {
+            mainNavWrapper.classList.remove('h-0');
+            mainNavWrapper.style.height = `${mainNavHeight}px`;
+            mainNav.setAttribute('aria-hidden', 'false');
+            menuBtnTxt.textContent = "Tanca el men\xfa principal";
+            this.classList.add('tv-main-nav__toggle--close');
+            mainNavLinks.forEach((link)=>{
+                link.setAttribute('tabindex', '0');
+            });
+        } else {
+            mainNavWrapper.style.height = '0';
+            mainNav.setAttribute('aria-hidden', 'true');
+            menuBtnTxt.textContent = "Mostra el men\xfa principal";
+            this.classList.remove('tv-main-nav__toggle--close');
+            mainNavLinks.forEach((link)=>{
+                link.setAttribute('tabindex', '-1');
+            });
+            setTimeout(()=>{
+                mainNavWrapper.classList.add('h-0');
+            }, 300); // Match the transition duration
+        }
+    });
     // Viewport Min Height
     const viewport = document.querySelector('#tv-viewport'), footer = document.querySelector('#tv-footer'), footerHeight = footer ? footer.offsetHeight : 0;
     if (viewport) viewport.style.setProperty('--tv-footer-height', footerHeight + 'px');
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,47 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 
+    // Main Navigation Toggle
+
+    const mainNav = document.querySelector('.tv-main-nav'),
+        mainNavWrapper = document.querySelector('.tv-main-nav__wrapper'),
+        menuBtn = document.querySelector('.tv-main-nav__toggle'),
+        menuBtnTxt = menuBtn ? menuBtn.querySelector('.tv-main-nav__toggle__txt') : null,
+        mainNavHeight = mainNav.offsetHeight,
+        mainNavLinks = mainNav.querySelectorAll('a');
+
+    if (menuBtn && mainNav) {
+        
+        menuBtn.addEventListener('click', function() {
+            if (mainNavWrapper.classList.contains('h-0')) {
+                mainNavWrapper.classList.remove('h-0');
+                mainNavWrapper.style.height = `${mainNavHeight}px`;
+                mainNav.setAttribute('aria-hidden', 'false');
+                menuBtnTxt.textContent = 'Tanca el menú principal';
+                this.classList.add('tv-main-nav__toggle--close');
+
+                mainNavLinks.forEach( link => {
+                    link.setAttribute('tabindex', '0');
+                } );
+
+            } else {
+                mainNavWrapper.style.height = '0';
+                mainNav.setAttribute('aria-hidden', 'true');
+                menuBtnTxt.textContent = 'Mostra el menú principal';
+                this.classList.remove('tv-main-nav__toggle--close');
+
+                mainNavLinks.forEach( link => {
+                    link.setAttribute('tabindex', '-1');
+                } );
+
+                setTimeout(() => {
+                    mainNavWrapper.classList.add('h-0');
+                }, 300); // Match the transition duration
+            }
+        });
+
+    }
+
 
     // Viewport Min Height
 
