c0a86709c5a5a07cf4482388ac671474129068a4
Jordi <hola@jordimoreno.cat>
2025-10-23 10:46:16 +0200

    Undo sticky footer

--- src/js/app.js
+++ src/js/app.js
@@ -53,22 +53,4 @@ window.addEventListener('DOMContentLoaded', function() {
             viewport.style.setProperty('--tv-footer-height', footerHeight + 'px');
         }
 
-
-    // Footer A11y
-
-    const footerLinks = footer.querySelectorAll('button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)'),
-        body = document.querySelector('body');
-
-    footerLinks.forEach( link => {
-
-        link.addEventListener( 'focus', function() {
-
-            console.log('focuseeed');
-
-            body.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-        } )
-
-    } )
-
 });
